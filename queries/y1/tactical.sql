003767446b1556c73a03e44469b048b1f48b058a
WaiYi1612 <76613496+WaiYi1612@users.noreply.github.com>
2021-08-25 01:48:51 +0800

    query

---
+++ queries/y1/tactical.sql
@@ -0,0 +1,27 @@
+--Author: Wong Wai Yi
+
+SET TERMOUT OFF 
+SET LINESIZE 120
+SET PAGESIZE 100
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY';
+SET TERMOUT ON
+
+COLUMN delivery_date FORMAT A20 HEADING 'Delivery Date';
+COLUMN branch FORMAT A20 HEADING 'Branch';
+COLUMN total_parcel FORMAT 9999 HEADING 'Total Parcel';
+
+PROMPT Every branch delivery quantity per month
+ACCEPT d_date_start PROMPT 'Enter a Start Date wish to search: '
+ACCEPT d_date_end PROMPT 'Enter a End Date wish to search: '
+PROMPT
+
+BREAK ON delivery_date ON branch
+SELECT D.delivery_date, S.branch, COUNT(P.parcel_id) AS total_parcel
+FROM "Staff" S, "Delivery" D, "Parcel" P
+WHERE S.staff_id = D.staff_id AND D.delivery_id = P.delivery_id AND
+    delivery_date BETWEEN TO_DATE('&d_date_start', 'DD/MM/YYYY') AND TO_DATE('&d_date_end', 'DD/MM/YYYY')
+GROUP BY D.delivery_date, S.branch
+ORDER BY D.delivery_date;
+
+CLEAR COLUMN COLUMNS
+CLEAR BREAKS
