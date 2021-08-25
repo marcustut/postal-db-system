f98f024a33f2e47f4a5f1a5f284f8413e2469794
WaiYi1612 <76613496+WaiYi1612@users.noreply.github.com>
2021-08-25 17:40:52 +0800

    Update tactical.sql

--- queries/y1/tactical.sql
+++ queries/y1/tactical.sql
@@ -17,13 +17,19 @@ ACCEPT d_date_start PROMPT 'Enter a Start Date wish to search: '
 ACCEPT d_date_end PROMPT 'Enter a End Date wish to search: '
 PROMPT
 
-BREAK ON delivery_date ON branch
-SELECT D.delivery_date, S.branch, COUNT(P.parcel_id) AS total_parcel
+CREATE OR REPLACE VIEW TOTAL_DAILY_PARCEL_VIEW AS
+SELECT D.delivery_id, D.delivery_date, S.branch, COUNT(P.parcel_id) AS total_parcel
 FROM "Staff" S, "Delivery" D, "Parcel" P
-WHERE S.staff_id = D.staff_id AND D.delivery_id = P.delivery_id AND
-    delivery_date BETWEEN TO_DATE('&d_date_start', 'DD/MM/YYYY') AND TO_DATE('&d_date_end', 'DD/MM/YYYY')
-GROUP BY D.delivery_date, S.branch
-ORDER BY D.delivery_date, total_parcel DESC;
+WHERE S.staff_id = D.staff_id AND D.delivery_id = P.delivery_id 
+GROUP BY D.delivery_id, D.delivery_date, S.branch;
+
+BREAK ON delivery_date ON branch
+SELECT D.delivery_date, V.branch, MAX(V.total_parcel) AS Total
+FROM TOTAL_DAILY_PARCEL_VIEW V, "Delivery" D
+WHERE D.delivery_id = V.delivery_id AND 
+D.delivery_date BETWEEN TO_DATE('&d_date_start', 'DD/MM/YYYY') AND TO_DATE('&d_date_end', 'DD/MM/YYYY')
+GROUP BY D.delivery_date, V.branch
+ORDER BY D.delivery_date, Total DESC;
 
 CLEAR COLUMN COLUMNS
 CLEAR BREAKS
