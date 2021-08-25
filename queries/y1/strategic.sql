d2137090a1e9b2edcdeb97fcc0403d4c922944b3
WaiYi1612 <76613496+WaiYi1612@users.noreply.github.com>
2021-08-25 09:09:15 +0800

    Query

---
+++ queries/y1/strategic.sql
@@ -0,0 +1,30 @@
+SET TERMOUT OFF 
+SET LINESIZE 90
+SET PAGESIZE 100
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY';
+SET TERMOUT ON
+
+TTITLE CENTER 'Top 10 Staff in Year' SKIP 2
+
+COLUMN branch FORMAT A20 HEADING 'Branch' JUSTIFY CENTER
+COLUMN staff_id FORMAT 99999 HEADING 'Staff Id' JUSTIFY CENTER
+COLUMN staff_name FORMAT A30 HEADING 'Staff Name' JUSTIFY CENTER
+COLUMN total_parcel FORMAT 9999 HEADING 'Total Parcel' JUSTIFY CENTER
+
+COMPUTE SUM LABEL "Total: " OF total_parcel ON REPORT
+BREAK ON REPORT
+
+ACCEPT d_year number PROMPT 'Enter a Year: '
+
+SELECT S.branch, S.staff_id, S.staff_name, COUNT(P.parcel_id) AS total_parcel
+FROM "Staff" S, "Delivery" D, "Parcel" P
+WHERE S.staff_id = D.staff_id AND D.delivery_id = P.delivery_id 
+AND EXTRACT(YEAR FROM D.delivery_date) = &d_year
+GROUP BY S.branch, S.staff_id, S.staff_name
+ORDER BY total_parcel DESC
+FETCH FIRST 10 ROWS ONLY;
+
+CLEAR COLUMNS
+CLEAR BREAKS
+CLEAR COMPUTES
+TTITLE OFF
--- queries/y1/tactical.sql
+++ queries/y1/tactical.sql
@@ -6,9 +6,11 @@ SET PAGESIZE 100
 ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY';
 SET TERMOUT ON
 
+TTITLE CENTER 'Highest Number of Delivery Daily' SKIP 2
+
 COLUMN delivery_date FORMAT A20 HEADING 'Delivery Date';
 COLUMN branch FORMAT A20 HEADING 'Branch';
-COLUMN total_parcel FORMAT 9999 HEADING 'Total Parcel';
+COLUMN total FORMAT 9999 HEADING 'Total Parcel';
 
 PROMPT Every branch delivery quantity per month
 ACCEPT d_date_start PROMPT 'Enter a Start Date wish to search: '
@@ -21,7 +23,8 @@ FROM "Staff" S, "Delivery" D, "Parcel" P
 WHERE S.staff_id = D.staff_id AND D.delivery_id = P.delivery_id AND
     delivery_date BETWEEN TO_DATE('&d_date_start', 'DD/MM/YYYY') AND TO_DATE('&d_date_end', 'DD/MM/YYYY')
 GROUP BY D.delivery_date, S.branch
-ORDER BY D.delivery_date;
+ORDER BY D.delivery_date, total_parcel DESC;
 
 CLEAR COLUMN COLUMNS
 CLEAR BREAKS
+TTITLE OFF
