98c6add20e755e23df92c9cdc01c180aee41c15a
WaiYi1612 <76613496+WaiYi1612@users.noreply.github.com>
2021-08-31 07:52:59 +0800

    Error handling

--- queries/y1/summary.sql
+++ queries/y1/summary.sql
@@ -19,7 +19,12 @@ i_ins_percent number;
 i_total_percent number:=0;
 i_total_type number:=0; 
 i_total_claim number:=0;
+E_NO_YEAR_FOUND EXCEPTION;
+PRAGMA EXCEPTION_INIT (E_NO_YEAR_FOUND, -20018);
 
+CURSOR YEAR_CURSOR IS 
+    SELECT * FROM "Parcel" WHERE EXTRACT(YEAR FROM updated_at) = IN_YEAR;
+YEAR_REC YEAR_CURSOR%ROWTYPE;
 
 CURSOR INSURANCE_ORDER_CURSOR IS 
     SELECT I.insurance_id AS "i_ins_id", I.type AS "i_ins_type", I.price AS "i_ins_price", V.i_claim as "i_claim_no", COUNT(I.type) AS "i_type_no", COUNT(I.type)/(
@@ -32,6 +37,12 @@ CURSOR INSURANCE_ORDER_CURSOR IS
 ins_rec INSURANCE_ORDER_CURSOR%ROWTYPE;
 
 BEGIN
+    OPEN YEAR_CURSOR;
+    FETCH YEAR_CURSOR INTO YEAR_REC;
+    IF (YEAR_CURSOR%NOTFOUND) THEN
+     RAISE E_NO_YEAR_FOUND;
+     CLOSE YEAR_CURSOR;
+     END IF;
     DBMS_OUTPUT.PUT_LINE(RPAD('DATE',10,' ')||':'||SYSDATE);
     DBMS_OUTPUT.PUT_LINE(RPAD('DAY',10,' ')||':'||TO_CHAR(SYSDATE,'DAY'));
     DBMS_OUTPUT.PUT_LINE(chr(10));
@@ -93,6 +104,11 @@ DBMS_OUTPUT.PUT_LINE(
         RPAD(' ',12,' ')||
         RPAD(i_total_claim,10,' ')
         );
+    EXCEPTION
+    WHEN E_NO_YEAR_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('--------------------------');
+        DBMS_OUTPUT.PUT_LINE('No Record Found in '||IN_YEAR);
+        DBMS_OUTPUT.PUT_LINE('--------------------------');
     END;
     /
 
